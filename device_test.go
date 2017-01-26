79abb76bb821cbd6606deb6df31b22b836640af1
Marshall Brekka <marshallsmedia@gmail.com>
2017-01-25 19:15:05 -0800

    test basic device commands (Open, Close, Version).

---
+++ device_test.go
@@ -0,0 +1,99 @@
+package u2fhost
+
+import (
+	"errors"
+	"testing"
+)
+
+// Implement hid.Device interface
+type testDevice struct {
+	// apdu request params
+	instruction uint8
+	p1          uint8
+	p2          uint8
+	request     []byte
+
+	// apdu response elements
+	status   uint16
+	response []byte
+	error    error
+
+	// open error
+	openError error
+}
+
+func (d *testDevice) Open() error {
+	return d.openError
+}
+
+func (d *testDevice) Close() {}
+
+func (d *testDevice) SendAPDU(instruction, p1, p2 uint8, data []byte) (uint16, []byte, error) {
+	d.instruction = instruction
+	d.p1 = p1
+	d.p2 = p2
+	d.request = data
+	return d.status, d.response, d.error
+}
+
+func TestOpen(t *testing.T) {
+	testHid := &testDevice{}
+	dev := newHidDevice(testHid)
+
+	err := dev.Open()
+	if err != nil {
+		t.Fatalf("Unexpected error when opening device: %s", err)
+	}
+
+	openErr := errors.New("Open Error")
+	testHid.openError = openErr
+
+	err = dev.Open()
+	if err == nil {
+		t.Fatalf("Expected error when opening device")
+	} else if err != openErr {
+		t.Fatalf("Expected error %s but got %s", openErr, err)
+	}
+}
+
+// Kind of pointless, but was easy to add
+func TestClose(t *testing.T) {
+	testHid := &testDevice{}
+	dev := newHidDevice(testHid)
+
+	dev.Close()
+}
+
+func TestVersion(t *testing.T) {
+	// Happy path
+	testHid := &testDevice{
+		response: []byte("U2F_V2"),
+		status:   u2fStatusNoError,
+	}
+	dev := newHidDevice(testHid)
+	version, err := dev.Version()
+	if err != nil {
+		t.Errorf("Unexpected error getting version: %s", err)
+	} else if version != "U2F_V2" {
+		t.Errorf("Expected version U2F_V2 but got %s", version)
+	}
+
+	// Error from SendAPDU
+	testHid.error = errors.New("APDU Error")
+	version, err = dev.Version()
+	if err == nil {
+		t.Errorf("Expected error when getting version, but did not get one.")
+	} else if err != testHid.error {
+		t.Errorf("Expected error `%s` got `%s`", testHid.error, err)
+	}
+
+	// Error from status code
+	testHid.error = nil
+	testHid.status = u2fStatusCommandNotAllowed
+	version, err = dev.Version()
+	if err == nil {
+		t.Errorf("Expected error when getting version, but did not get one.")
+	} else if err.Error() != "U2FError: 0x6986" {
+		t.Errorf("Expected error `U2FError: 0x6986` got `%s`", err)
+	}
+}
